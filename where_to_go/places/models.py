7d5d130e99a34498c11bc809f535512dcbc94925
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-10 22:35:54 +0600

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -10,3 +10,12 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    order = models.IntegerField()
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+    def __str__(self):
+        return f'{self.order} {self.place.title}'
