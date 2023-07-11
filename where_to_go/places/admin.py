1d91a369158b886f20aee82b38fe9d94e00d390c
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-11 18:30:14 +0600

    Add previews

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['get_image']
+
+    def get_image(self, obj):
+        url = obj.image.url
+        max_height = 200
+        ratio = obj.image.height / max_height
+        width = round(obj.image.width / ratio)
+        height = round(obj.image.height / ratio)
+        return format_html(
+            f'<img src="{url}" width="{width}" height={height} />'
+        )
 
 
 @admin.register(Place)
