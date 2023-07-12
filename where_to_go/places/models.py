8ebca797ebd9101c2b6392646a6017ed48e14329
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-12 08:17:07 +0600

    Add WYSIWYG editor

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=100)
     description_short = models.TextField()
-    description_long = models.TextField()
+    description_long = HTMLField()
     longitude = models.FloatField()
     latitude = models.FloatField()
 
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import show_index, show_place
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_index),
-    path('place/<int:place_id>', show_place, name='show_place')
+    path('place/<int:place_id>', show_place, name='show_place'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
