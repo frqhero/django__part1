4ebfc0db765196c8fef61e26261e2f2a4e95fbcf
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-12 08:46:01 +0600

    Prepare the project for production deployment

---
+++ readme.md
@@ -0,0 +1,5 @@
+env vars  
+`.env` file should be placed in the root of the project  
+`DEBUG` - false by default  
+`ALLOWED_HOSTS` - list (e.g. *,localhost,website.ru) required if debug false, [] by default 
+`SECRET_KEY` - 'REPLACE_ME' by default
--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1c!4lq7s$*@+t@32x#)vgajddt(mtz12=0qz8+ecupdg1hs&h-'
+SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 
 # Application definition
