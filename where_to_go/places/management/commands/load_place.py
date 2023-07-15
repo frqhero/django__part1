cbe11a6c3f8224aed0df33c935ba5625074ce9d1
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-15 13:02:48 +0600

    Add 'load_place' management command

---
+++ where_to_go/places/management/commands/load_place.py
@@ -0,0 +1,46 @@
+from urllib.parse import urlparse, unquote
+import os
+
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+import requests
+
+from places.models import Place, Image
+
+
+def remove_places_images(queryset):
+    for image in queryset:
+        os.remove(image.image.file.name)
+        image.delete()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        response = requests.get(args[0]).json()
+        place, created = Place.objects.get_or_create(
+            title=response['title'],
+            defaults={
+                'description_short': response['description_short'],
+                'description_long': response['description_long'],
+                'latitude': response['coordinates']['lat'],
+                'longitude': response['coordinates']['lng'],
+            },
+        )
+        if not created:
+            remove_places_images(place.images.all())
+        for count, img in enumerate(response['imgs']):
+            filename = os.path.basename(unquote(urlparse(img).path))
+            image_response = requests.get(img)
+            img_model = Image()
+            img_model.image.save(
+                filename, ContentFile(image_response.content), save=False
+            )
+            img_model.place = place
+            img_model.order = count
+            img_model.save()
+            1
+
+        1
+
+    def add_arguments(self, parser):
+        parser.add_argument(nargs='+', type=str, dest='args')
