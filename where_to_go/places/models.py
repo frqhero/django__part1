cd38406e28d2e274c3a9ca76c3792a5b01fb6497
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-25 10:07:11 +0600

    Add verbose name for some fields in models

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField(max_length=100)
     short_description = models.TextField(blank=True)
     long_description = HTMLField(blank=True)
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    longitude = models.FloatField(verbose_name='Долгота')
+    latitude = models.FloatField(verbose_name='Широта')
 
     def __str__(self):
         return self.title
@@ -17,13 +17,14 @@ class Image(models.Model):
     class Meta:
         ordering = ['order']
 
-    order = models.IntegerField(default=0)
+    order = models.IntegerField(default=0, verbose_name='Очередность')
     place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
-        related_name='images'
+        related_name='images',
+        verbose_name='Место'
     )
-    image = models.ImageField()
+    image = models.ImageField(verbose_name='Изображение')
 
     def __str__(self):
         return f'{self.order} {self.place.title}'
