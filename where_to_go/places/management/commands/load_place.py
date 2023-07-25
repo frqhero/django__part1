39b91fffe9fe9a64f9af9e344c0389a3ead9e9bd
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-25 10:35:59 +0600

    Prevent circular import

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -1,45 +1,11 @@
-from urllib.parse import urlparse, unquote
-import os
-
-from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
-import requests
 
-from places.models import Place, Image
-
-
-def remove_places_images(queryset):
-    for image in queryset:
-        os.remove(image.image.file.name)
-        image.delete()
+from .loading_place import load_place
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        response = requests.get(args[0])
-        response.raise_for_status()
-        response = response.json()
-        place, created = Place.objects.get_or_create(
-            title=response['title'],
-            defaults={
-                'description_short': response['description_short'],
-                'description_long': response['description_long'],
-                'latitude': response['coordinates']['lat'],
-                'longitude': response['coordinates']['lng'],
-            },
-        )
-        if not created:
-            remove_places_images(place.images.all())
-        for count, img in enumerate(response['imgs']):
-            filename = os.path.basename(unquote(urlparse(img).path))
-            image_response = requests.get(img)
-            img_model = Image()
-            img_model.image.save(
-                filename, ContentFile(image_response.content), save=False
-            )
-            img_model.place = place
-            img_model.order = count
-            img_model.save()
+        load_place(args[0])
 
     def add_arguments(self, parser):
         parser.add_argument(nargs='+', type=str, dest='args')
--- where_to_go/places/management/commands/load_places_from_file.py
+++ where_to_go/places/management/commands/load_places_from_file.py
@@ -1,9 +1,7 @@
-import os
-
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from .load_place import Command as LoadPlaceCommand
+from .loading_place import load_place
 
 
 class Command(BaseCommand):
@@ -12,4 +10,4 @@ class Command(BaseCommand):
         static_dir = settings.STATICFILES_DIRS[static_folder]
         with open(f'{static_dir}/places.txt', 'r') as file:
             links = [place.strip() for place in file.readlines()]
-        [LoadPlaceCommand.handle('_', link) for link in links]
+        [load_place(link) for link in links]
